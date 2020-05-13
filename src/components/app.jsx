c56ca5c28cf501089cfcd557d8c728fcff286dca
Maroussia Arnault <maroussia.arnault@gmail.com>
2020-05-13 17:27:17 +0200

    Creation of a components files

---
+++ src/components/app.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return(
+      <div>
+        <div className="left-scene">left-scene</div>
+        <div className="right-scene">right-scene</div>
+      </div>
+    );
+  }
+}
+
+export default App;
