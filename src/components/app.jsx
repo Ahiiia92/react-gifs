633387ada3842da8affb70e0ec7a6c048e6ff997
Maroussia Arnault <maroussia.arnault@gmail.com>
2020-05-13 18:38:45 +0200

    components added

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
 
+import SearchBar from './search_bar.jsx';
+import Gif from './gif.jsx';
+import GifList from './gif_list.jsx';
+
 class App extends Component {
   render() {
+    const gifs = [
+      { id: "Ll1rEkDebTIdO"},
+      { id: "Wn74RUT0vjnoU98Hnt"}
+    ];
     return(
       <div>
-        <div className="left-scene">left-scene</div>
-        <div className="right-scene">right-scene</div>
+        <div className="left-scene">
+          <SearchBar />
+          <div className="selected-gif">
+            <Gif id={gifs[0].id} />
+          </div>
+        </div>
+        <div className="right-scene">
+          <GifList gifs={gifs} />
+        </div>
       </div>
     );
   }
